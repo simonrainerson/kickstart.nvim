91ee2a7b8612a96442c483a3fe7942fc1a685c74
Simon Rainerson <simon.rainerson@goodbyekansas.com>
2024-04-06 02:41:31 +0200

    🔱 Add harpoon keymaps

--- init.lua
+++ init.lua
@@ -182,7 +182,44 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
-  'theprimeagen/harpoon', -- Jump between files easily
+  {
+    'theprimeagen/harpoon', -- Jump between files easily
+    config = function()
+      local mark = require 'harpoon.mark'
+      local ui = require 'harpoon.ui'
+
+      vim.keymap.set('n', '<leader>a', mark.add_file)
+      vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+
+      vim.keymap.set('n', '<leader>1', function()
+        ui.nav_file(1)
+      end)
+      vim.keymap.set('n', '<leader>2', function()
+        ui.nav_file(2)
+      end)
+      vim.keymap.set('n', '<leader>3', function()
+        ui.nav_file(3)
+      end)
+      vim.keymap.set('n', '<leader>4', function()
+        ui.nav_file(4)
+      end)
+      vim.keymap.set('n', '<leader>5', function()
+        ui.nav_file(5)
+      end)
+      vim.keymap.set('n', '<leader>6', function()
+        ui.nav_file(6)
+      end)
+      vim.keymap.set('n', '<leader>7', function()
+        ui.nav_file(7)
+      end)
+      vim.keymap.set('n', '<leader>8', function()
+        ui.nav_file(8)
+      end)
+      vim.keymap.set('n', '<leader>9', function()
+        ui.nav_file(9)
+      end)
+    end,
+  },
 
   'mbbill/undotree', -- Branching undo history
 
